ce9555780856d8acdc2ac1e38632d526cfbc4d5e
emablekos <emablekos@gmail.com>
2016-08-14 11:07:23 -0400

    Initial Commit

---
+++ BioInformatics/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BioInformatics
+//
+//  Created by E.J. Mablekos on 8/14/16.
+//  Copyright © 2016 EJ Mablekos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
